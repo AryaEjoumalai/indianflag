2e01744c44a65fadd564f8055ab3430154a307b0
AryaEjoumalai <100434905+AryaEjoumalai@users.noreply.github.com>
2023-08-26 12:50:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const wheel=document.getElementsByClassName("wheel")[0];
+document.addEventListener("DOMContentLoaded", function() {
+  const wheel = document.getElementsByClassName("wheel")[0];
 
-for (let i = 0; i < 12; i++) {
-  let line = document.createElement("div");
-  line.className = "line";
-  line.style.transform = "rotate("+i*15+"deg)";
-  wheel.appendChild(line);
-}
+  for (let i = 0; i < 12; i++) {
+    let line = document.createElement("div");
+    line.className = "line";
+    line.style.transform = "rotate(" + i * 15 + "deg)";
+    wheel.appendChild(line);
+  }
+});
